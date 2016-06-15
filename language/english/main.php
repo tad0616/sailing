8f689515e1c5769fe7bb3a45a5b3018aafbedb1a
Tad <tad0616@gmail.com>
2016-06-15 15:22:33 +0800

    修正引入語系的全域變數

--- language/english/main.php
+++ language/english/main.php
@@ -1,5 +1,5 @@
 <?php
-include_once XOOPS_ROOT_PATH . '/modules/tadtools/language/' . $xoopsConfig['language'] . '/theme_common.php';
+include_once XOOPS_ROOT_PATH . '/modules/tadtools/language/' . $GLOBALS['xoopsConfig']['language'] . '/theme_common.php';
 
 /**自訂設定之語系**/
 define("TF_SHOW_VAR", "Display themes vars.");
